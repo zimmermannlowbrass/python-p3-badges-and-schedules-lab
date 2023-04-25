b2e1a8ca69dc6cf337c41feb5ff833573a268817
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-25 16:34:55 -0500

    completed labwork

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assigns = []
+    for x in range(len(names)):
+        # y = x % 7
+        room_assigns.append(f"Hello, {names[x]}! You'll be assigned to room {x + 1}!")
+    return room_assigns
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
+
+
+print(printer(["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]))
